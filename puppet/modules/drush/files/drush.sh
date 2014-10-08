50b643bb8fa85816afb91c1218df15959adadbee
Adrian Mak <adrian_mak@msn.com>
2014-10-08 18:27:05 +0800

    updated

--- puppet/modules/drush/files/drush.sh
+++ puppet/modules/drush/files/drush.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/local/bin/composer global require drush/drush:dev-master
 sudo ln -sf $HOME/.composer/vendor/drush/drush /usr/bin/drush
-echo "export PATH=$PATH:/usr/bin/drush" >> $HOME/.bashrc
+echo 'export PATH="$PATH:/usr/bin/drush"' >> $HOME/.bashrc
